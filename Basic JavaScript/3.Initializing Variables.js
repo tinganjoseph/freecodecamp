191ee9c7030b4e46abed5e6455802d0260e7acbe
tinganjoseph <tinganjoseph7@gmail.com>
2023-08-04 11:24:02 +0000

    initializing variables

---
+++ Basic JavaScript/3.Initializing Variables.js
@@ -0,0 +1,6 @@
+/**
+ * todo: Initializing Variables with the Assignment Operator
+  * ? It is common to initialize a variable to an initial value in the same line as it is declared.
+ */
+
+var myAge= 13;
