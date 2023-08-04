07ced2fbf9a7c5f657c2826a82f79058e86851ff
tinganjoseph <tinganjoseph7@gmail.com>
2023-08-04 11:21:39 +0000

    assigning the value of one variable to another

--- Basic JavaScript/2.Storing Values.js
+++ Basic JavaScript/2.Storing Values.js
@@ -3,3 +3,13 @@
  */
 
 myVariable = 5;
+
+
+/**
+ * * Assigning the Value of One Variable to Another After a value is assigned to a variable using the assignment operator, you can assign the value of that variable to another variable using the assignment operator.
+ */
+
+var myVar;
+myVar = 5;
+var myNum;
+myNum = myVar;
